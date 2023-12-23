92f7d836c7bfa0d7b4308d37619faa9595434c5f
unbelievable <124208377+BlessedMisha@users.noreply.github.com>
2023-12-23 12:13:55 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -8,35 +8,6 @@ navLeft.addEventListener('click', () => {
     iconClose.classList.toggle('hide')
     menu.classList.toggle('hide')
 })
-function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
-
-function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
-
-
-    
-
-
-window.addEventListener('scroll', animateOnScroll);
-
-document.addEventListener('DOMContentLoaded', animateOnScroll);
-
-
 
 window.addEventListener("scroll", function () {
     const navbar = document.getElementById("myNavbar");
@@ -52,27 +23,6 @@ window.addEventListener("scroll", function () {
 ScrollReveal({ delay: 250 }).reveal(".scrollreveal");
 ScrollReveal({ delay: 3 }).reveal(".scrollreveal-noreturn");
 
-document.addEventListener("DOMContentLoaded", function () {
-
-    var body = document.body;
-    setInterval(createStar, 100);
-    function createStar() {
-        var right = Math.random() * 500;
-        var top = Math.random() * screen.height;
-        var star = document.createElement("div");
-        star.classList.add("star")
-        body.appendChild(star);
-        setInterval(runStar, 10);
-        star.style.top = top + "px";
-        function runStar() {
-            if (right >= screen.width) {
-                star.remove();
-            }
-            right += 3;
-            star.style.right = right + "px";
-        }
-    }
-})
 
 var toggleButton = document.getElementById('centered-toggle-button');
 var modal = document.getElementById('myModal');
